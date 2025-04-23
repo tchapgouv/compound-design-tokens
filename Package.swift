4986912989b6d7fd63f108d8c33961acb9b71f53
Nicolas Buquet <nbuquet@buquet-net.com>
2025-04-23 17:34:46 +0200

    Swift Package: embed fonts as a folder (copy will keep hierarchy)

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
         .library(name: "CompoundDesignTokens", targets: ["CompoundDesignTokens"])
     ],
     targets: [
-        .target(name: "CompoundDesignTokens", path: "assets/ios/swift", resources: [.process("fonts")])
+        .target(name: "CompoundDesignTokens", path: "assets/ios/swift", resources: [.copy("fonts")])
     ]
 )
