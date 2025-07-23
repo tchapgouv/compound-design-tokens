706c83d7222d420ed5033acad5d01a89a9a3d134
ElementRobot <releases@riot.im>
2025-07-23 09:23:05 +0100

    Sync icons from Figma (#174)
    * Sync icons from Figma
    
    * Remove stale assets.
    
    ---------
    
    Co-authored-by: robintown <48614497+robintown@users.noreply.github.com>
    Co-authored-by: Doug <douglase@element.io>

--- assets/web/icons/exit-full-screen.d.ts
+++ assets/web/icons/exit-full-screen.d.ts
@@ -1,11 +1,21 @@
 import React from "react";
 
 /**
+<<<<<<<< HEAD:assets/web/icons/exit-full-screen.d.ts
  * exit-full-screen.svg
  */
 declare const ExitFullScreenIcon: React.ForwardRefExoticComponent<
+========
+ * space-solid.svg
+ */
+declare const SpaceSolidIcon: React.ForwardRefExoticComponent<
+>>>>>>>> be5d26d (Sync icons from Figma (#174)):assets/web/icons/space-solid.d.ts
     Omit<React.SVGProps<SVGSVGElement>, "ref" | "children"> &
     React.RefAttributes<SVGSVGElement>
 >;
 
+<<<<<<<< HEAD:assets/web/icons/exit-full-screen.d.ts
 export default ExitFullScreenIcon;
+========
+export default SpaceSolidIcon;
+>>>>>>>> be5d26d (Sync icons from Figma (#174)):assets/web/icons/space-solid.d.ts
