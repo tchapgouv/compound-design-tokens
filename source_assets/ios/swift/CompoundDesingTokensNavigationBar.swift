061f39a0e6111a9d095985ece2c5afefb6226801
Nicolas Buquet <github@buquet-net.com>
2025-09-29 15:05:18 +0200

    Gérer la fonte custom Tchap dans la NavBar à l'installation du dégradé.

---
+++ source_assets/ios/swift/CompoundDesingTokensNavigationBar.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  CompoundDesignTokens
+//
+//  Created by Nicolas Buquet on 29/09/2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension UINavigationBarAppearance {
+    fileprivate static let navigationBarTitleFont = UIFont(name: "Marianne-Medium", size: 16.0)!
+    fileprivate static let navigationBarLargeTitleFont = UIFont(name: "Marianne-Medium", size: 21.0)!
+    
+    public func withTchapFonts() -> Self {
+        self.titleTextAttributes = [.font: UIFont(name: "Marianne-Medium", size: 16.0)!]
+        self.largeTitleTextAttributes = [.font: UIFont(name: "Marianne-Medium", size: 21.0)!]
+        return self
+    }
+}
